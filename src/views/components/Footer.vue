a30ef921461a3bc50b6274de47ec8e3b092e3b1b
ModernAmusements <st@zahnarzt-helden.de>
2021-04-01 20:03:12 +0200

    porduction

--- src/views/components/Footer.vue
+++ src/views/components/Footer.vue
@@ -1,43 +1,49 @@
 <template>
-    <div class="footer">
-        <p class="format-normal-klein">
-            <strong>Inhaltliche Verantwortung</strong><br /> Hochschule D&uuml;sseldorf<br /> Peter Behrens School of
-            Arts<br /> Georg–Glock–Stra&szlig;e 15<br /> 40474 D&uuml;sseldorf<br />
-            <a href="mailto:info@abschluss-hsd.de">info@abschluss-hsd.de</a><br />
-        </p>
-        <p class="format-normal-klein">
-            <strong>Gestaltung und Organisation</strong><br /> Kirsten Bramlage<br />
-            <a href="http://yanikhauschild.de/" target="_blank">Yanik Hauschild</a><br />
-            <a href="http://christianlindermann.de/" target="_blank">Christian Lindermann</a><br /><br />
-            <strong>Website</strong><br />
-            <a href="https://shady-tawfik.de" target="_blank">Shady Tawfik</a><br /><br />
-            <strong>Schrift</strong><br /> Blow von <a href="http://yanikhauschild.de" target="_blank">Yanik
-                Hauschild</a><br /> Format Grotesk von <a href="http://dominikbissem.tumblr.com/"
-                target="_blank">Dominik Bissem</a>
-        </p>
-        <span class="cf"></span>
-        <br /><br />
-        <div class="footer-logos">
-            <div class="footer-logos-left">
-                <a href="https://pbsa.hs-duesseldorf.de/" target="_blank"><img
-                        src="assets/images/logos/logos-hsd.svg" /></a>
-            </div>
-            <div class="footer-logos-right">
-                <a href="http://www.form.de/" target="_blank"><img src="assets/images/logos/logos-form.svg"
-                        alt="form" /></a>
-                <a href="http://novum.graphics/" target="_blank"><img src="assets/images/logos/logos-novum.svg"
-                        alt="novum" /></a>
-                <a href="http://page-online.de/" target="_blank"><img src="assets/images/logos/logos-page.svg"
-                        alt="page" /></a>
-            </div>
-        </div>
-        <span class="cf"></span>
+  <div class="footer">
+    <p class="format-normal-klein">
+      <strong>Inhaltliche Verantwortung</strong><br />
+      ecosign/Akademie für Gestaltung<br />
+      Nachhaltiges Design <br />
+      Gertrudenstraße 3<br />
+      20095 Hamburg<br />
+      <a href="mailto:info@ecosign.de">info@ecosign.de</a><br />
+    </p>
+    <p class="format-normal-klein">
+      <strong>Gestaltung und Organisation</strong><br />
+      Dennis Ackermann<br />
+      Kerstin Becker<br />
+      Anna Wurfel<br /><br />
+      <strong>Website</strong><br />
+      <a href="https://shady-tawfik.de" target="_blank">Shady Tawfik</a><br /><br />
+      <strong>Schrift</strong><br />
+      Future von Tanja Braun <br />
+      Format Grotesk von Tanja Braun
+    </p>
+    <span class="cf"></span>
+    <br /><br />
+    <div class="footer-logos">
+      <div class="footer-logos-left">
+       <img src="assets/images/logos/logo.gif"
+        />
+      </div>
+      <div class="footer-logos-right">
+        <a href="http://www.form.de/" target="_blank"
+          ><img src="assets/images/logos/logos-form.svg" alt="form"
+        /></a>
+        <a href="http://novum.graphics/" target="_blank"
+          ><img src="assets/images/logos/logos-novum.svg" alt="novum"
+        /></a>
+        <a href="http://page-online.de/" target="_blank"
+          ><img src="assets/images/logos/logos-page.svg" alt="page"
+        /></a>
+      </div>
     </div>
+    <span class="cf"></span>
+  </div>
 </template>
 <script>
 export default {
-  name: 'app-footer'
+  name: "app-footer",
 };
 </script>
-<style>
-</style>
+<style></style>
